bcda9c8b6b2d356bcdbfbb365fc02dddf15f167d
Jasvinder Singh <jasvinder_singh@harvard.edu>
2015-08-12 13:58:17 -0400

    Configured New Relic

--- scripts/install_new_relic.sh
+++ scripts/install_new_relic.sh
@@ -4,5 +4,5 @@ rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarc
 yum install -y newrelic-php5 || true
 
 export NR_INSTALL_SILENT=true
-export NR_INSTALL_KEY=your_new_relic_key_here
+export NR_INSTALL_KEY=ffb44616460f34e4b429068e7f8989cec46f30a1
 newrelic-install install
